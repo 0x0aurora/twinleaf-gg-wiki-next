96492f60aa47c50ecc9f4eac50df8ecadbac38c1
Aurora <113322467+0x0aurora@users.noreply.github.com>
2025-01-27 22:53:07 +1100

    fix: index padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Logo from "~/components/Logo.png";
 
 export default function Home() {
   return (
-    <main className="flex-1 self-stretch flex items-center justify-center">
+    <main className="flex-1 self-stretch flex items-center justify-center p-3">
       <div className="prose dark:prose-invert text-center">
         <Image
           alt="Twinleaf Wiki Logo"
